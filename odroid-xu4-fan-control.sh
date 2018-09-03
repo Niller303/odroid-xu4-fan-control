666f8cf778d09113c5a7073e9f367084a763f62a
Niller303 <2797716+Niller303@users.noreply.github.com>
2018-09-03 19:46:06 +0200

    Making a fan curve

--- odroid-xu4-fan-control.sh
+++ odroid-xu4-fan-control.sh
@@ -20,7 +20,6 @@ TEMPERATURE_FILE="/sys/devices/virtual/thermal/thermal_zone0/temp"
 FAN_MODE_FILE="/sys/devices/platform/pwm-fan/hwmon/hwmon0/automatic"
 FAN_SPEED_FILE="/sys/devices/platform/pwm-fan/hwmon/hwmon0/pwm1"
 TEST_EVERY=3 #seconds
-new_fan_speed_default=80
 LOGGER_NAME=odroid-xu4-fan-control
 
 #make sure after quiting script fan goes to auto control
@@ -55,26 +54,16 @@ do
   current_max_temp=`cat ${TEMPERATURE_FILE} | cut -d: -f2 | sort -nr | head -1`
   ${DEBUG} && logger -t $LOGGER_NAME "event: read_max; temp: ${current_max_temp}"
 
+  #Making that nice fan curve
   new_fan_speed=0
-  if (( ${current_max_temp} >= 75000 )); then
-    new_fan_speed=255
-  elif (( ${current_max_temp} >= 70000 )); then
-    new_fan_speed=200
-  elif (( ${current_max_temp} >= 68000 )); then
-    new_fan_speed=130
-  elif (( ${current_max_temp} >= 66000 )); then
-    new_fan_speed=80 
-  elif (( ${current_max_temp} >= 63000 )); then
-    new_fan_speed=80
-  elif (( ${current_max_temp} >= 60000 )); then
-    new_fan_speed=0
-  elif (( ${current_max_temp} >= 58000 )); then
-    new_fan_speed=0
-  elif (( ${current_max_temp} >= 55000 )); then
-    new_fan_speed=0
-  else
-    new_fan_speed=0
+  new_fan_speed=$(python -c "print(int(8.194+1.07621**($current_max_temp/1000)))")
+
+  if (($new_fan_speed > 255)); then
+     new_fan_speed=255;
+  elif (($new_fan_speed < 65)); then #Note here that around ~60 and under makes it go BEEP
+     new_fan_speed=65;
   fi
+
   ${DEBUG} && logger -t $LOGGER_NAME "event: adjust; speed: ${new_fan_speed}"
   echo ${new_fan_speed} > ${FAN_SPEED_FILE}
 
